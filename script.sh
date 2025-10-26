e32185c35cbec9c40281ab362926e00a42c4eec5
borvux <bennyjoram@gmail.com>
2025-10-26 00:09:28 -0500

    updating readme to index to match the root

--- script.sh
+++ script.sh
@@ -1,12 +1,44 @@
-find . -type f -name "*.md" ! -path "./README.md" | while read -r file; do
-  title=$(grep '^# ' "$file" | sed 's/# //')
+#!/bin/bash
 
-  front_matter="---
-layout: default
-title: ${title}
----"
+find . -type f -name "*.md" ! -path "./index.md" | while read -r file; do
+  
+  echo "Processing $file..."
+  filename_no_ext=$(basename "$file" .md) 
+  
+  parent_dir_name=$(basename "$(dirname "$file")") 
+  
+  parent_title=$(echo "$parent_dir_name" | sed 's/^[0-9]*-//') 
+  
+  parent_dir_cleaned=$(echo "$parent_title" | tr '[:upper:]' '[:lower:]' | sed 's/ //g') 
 
-  original_content=$(tail -n +2 "$file")
+  sed -i.bak '/\[Go back\]/d' "$file"
 
-  echo "${front_matter}\n${original_content}" > "$file"
+  if [ "$filename_no_ext" == "$parent_dir_cleaned" ]; then
+    awk -v s1="parent_title: Home" -v s2="parent_url: /" '
+      /---/ { count++ }
+      { print }
+      count == 1 && /^title:/ {
+        print s1
+        print s2
+      }
+    ' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+
+  else
+    parent_url="./${parent_dir_cleaned}.md"
+
+    awk -v s1="parent_title: $parent_title" -v s2="parent_url: $parent_url" '
+      /---/ { count++ }
+      { print }
+      count == 1 && /^title:/ {
+        print s1
+        print s2
+      }
+    ' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+
+  fi
+
+  rm -f "$file.bak"
+
 done
+
+echo "All markdown files have been updated!"
