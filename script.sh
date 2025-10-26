94eaa30e18281edaed17929a8233ce8172bb949c
borvux <bennyjoram@gmail.com>
2025-10-25 23:05:04 -0500

    adding jekyll configs and setups

--- script.sh
+++ script.sh
@@ -1,54 +1,12 @@
-#!/bin/bash
+find . -type f -name "*.md" ! -path "./README.md" | while read -r file; do
+  title=$(grep '^# ' "$file" | sed 's/# //')
 
-echo "Creating Old Testament directories and files..."
+  front_matter="---
+layout: default
+title: ${title}
+---"
 
-old_testament_books=(
-    "Genesis" "Exodus" "Leviticus" "Numbers" "Deuteronomy" "Joshua" "Judges" "Ruth" 
-    "1 Samuel" "2 Samuel" "1 Kings" "2 Kings" "1 Chronicles" "2 Chronicles" "Ezra" 
-    "Nehemiah" "Esther" "Job" "Psalms" "Proverbs" "Ecclesiastes" "Song of Solomon" 
-    "Isaiah" "Jeremiah" "Lamentations" "Ezekiel" "Daniel" "Hosea" "Joel" "Amos" 
-    "Obadiah" "Jonah" "Micah" "Nahum" "Habakkuk" "Zephaniah" "Haggai" "Zechariah" "Malachi"
-)
+  original_content=$(tail -n +2 "$file")
 
-main_ot_dir="1-Old-Testament"
-mkdir -p "${main_ot_dir}"
-
-i=1
-for book in "${old_testament_books[@]}"; do
-    num=$(printf "%02d" $i)
-    dir_path="${main_ot_dir}/${num}-${book}"
-    mkdir -p "${dir_path}"
-
-    file_name=$(echo "${book}" | tr '[:upper:]' '[:lower:]' | tr -d ' ')
-    file_path="${dir_path}/${file_name}.md"
-
-    echo "# ${book}" > "${file_path}"
-    
-    i=$((i+1))
-done
-
-echo "Creating New Testament directories and files..."
-
-new_testament_books=(
-    "Matthew" "Mark" "Luke" "John" "Acts" "Romans" "1 Corinthians" "2 Corinthians" 
-    "Galatians" "Ephesians" "Philippians" "Colossians" "1 Thessalonians" "2 Thessalonians" 
-    "1 Timothy" "2 Timothy" "Titus" "Philemon" "Hebrews" "James" "1 Peter" "2 Peter" 
-    "1 John" "2 John" "3 John" "Jude" "Revelation"
-)
-
-main_nt_dir="2-New-Testament"
-mkdir -p "${main_nt_dir}"
-
-j=1
-for book in "${new_testament_books[@]}"; do
-    num=$(printf "%02d" $j)
-    dir_path="${main_nt_dir}/${num}-${book}"
-    mkdir -p "${dir_path}"
-
-    file_name=$(echo "${book}" | tr '[:upper:]' '[:lower:]' | tr -d ' ')
-    file_path="${dir_path}/${file_name}.md"
-
-    echo "# ${book}" > "${file_path}"
-
-    j=$((j+1))
+  echo "${front_matter}\n${original_content}" > "$file"
 done
